fb3b9a7fc53463a527dfb88d5e99dfe5abe21693
caticastor <95300897+caticastor@users.noreply.github.com>
2021-11-30 13:58:45 -0600

    Add files via upload

---
+++ con_db.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbhost = "20.185.216.150";
+    $dbuser = "usercloud1";
+    $dbpass = "sercloud1";
+    $db = "info";
+   
+   // Create connection
+   $con = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+
+?>
