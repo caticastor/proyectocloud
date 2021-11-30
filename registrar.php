19b1e8127f59b220c89da41dadedff00a8287a9c
caticastor <95300897+caticastor@users.noreply.github.com>
2021-11-30 17:55:51 -0600

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -2,23 +2,23 @@
 
 include("con_db.php");
 
-if (isset($_POST['register'])) {
+if (isset($_POST['Guardar'])) {
     if (strlen($_POST['name']) >= 1) {
 	    $name = trim($_POST['name']);
 	    $consulta = "INSERT INTO telefono VALUES ('$name')";
 	    $resultado = mysqli_query($con,$consulta);
 	    if ($resultado) {
 	    	?> 
-	    	<h3>Nombre guardado correctamente</h3>
+	    	<h3>Número de teléfono guardado</h3>
            <?php
 	    } else {
 	    	?> 
-	    	<h3>No se pudo guardar el nombre, intente de nuevo</h3>
+	    	<h3>Se produjo un error, intente de nuevo</h3>
            <?php
 	    }
     }   else {
 	    	?> 
-	    	<h3>por favor complete el campo</h3>
+	    	<h3>El campo no puede estar vacío</h3>
            <?php
     }
 }
