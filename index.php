9489e6dac513af59789fbd435d3ff2475a770f7c
caticastor <95300897+caticastor@users.noreply.github.com>
2021-11-30 17:56:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registrar usuario</title>
+	<title>REGISTRO DE TELEFONOS</title>
 	<meta charset="utf-8">
 </head>
 <body>
     <form method="post">
-    	<input type="text" name="name" placeholder="Telefono">
-    	<input type="submit" name="register">
+    	<input type="text" name="num" placeholder="Telefono">
+    	<input type="submit" name="Guardar">
     </form>
         <?php 
         include("registrar.php");
