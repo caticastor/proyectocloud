67fdc8b4d4072cb6f53a67cab49c04a021df840d
caticastor <95300897+caticastor@users.noreply.github.com>
2021-11-30 18:07:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>REGISTRO DE TELEFONOS</title>
+	<title>Registro de Teléfono</title>
 	<meta charset="utf-8">
 </head>
 <body>
 	<center><h2>Captura tu número de teléfono</h2></center>
     <center>
 	<form method="post">
-    	<input type="text" name="num" placeholder="Telefono">
+    	<input type="text" name="num" placeholder="Teléfono">
 		<input type="submit" name="Guardar"></center>
     </form>
         <?php 
